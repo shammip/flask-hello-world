6b77cecbf7e44a615f0f523c6f5ad0a2e7f55d7c
shammip <shammirpereira@gmail.com>
2025-04-01 22:40:22 +0000

    create route db_select

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ def creating():
     conn = psycopg2.connect("postgresql://cspb3308data_user:cVEDPxhcrub7Rr1N4NaxpU1Vi8sl3sCy@dpg-cvm5lu9r0fns7380utf0-a/cspb3308data")
     cur = conn.cursor()
     cur.execute('''
-    CREATE TABLE IF NOT EXISTS Basketball(
+        CREATE TABLE IF NOT EXISTS Basketball(
         First varchar(255),
         Last varchar(255),
         City varchar(255),
         Name varchar(255),
         Number int
         );
-    ''')
+        ''')
     conn.commit()
     conn.close()
     return 'Basketball Table Successfully Created'
@@ -35,13 +35,33 @@ def inserting():
     conn = psycopg2.connect("postgresql://cspb3308data_user:cVEDPxhcrub7Rr1N4NaxpU1Vi8sl3sCy@dpg-cvm5lu9r0fns7380utf0-a/cspb3308data")
     cur = conn.cursor()
     cur.execute('''
-    INSERT INTO Basketball (First, Last, City, Name, Number)
+        INSERT INTO Basketball (First, Last, City, Name, Number)
         Values
         ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
         ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
         ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
         ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
-    ''')
+        ''')
     conn.commit()
     conn.close()
-    return 'Basketball Table Succesfully Populated'
+    return 'Basketball Table Successfully Populated'
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://cspb3308data_user:cVEDPxhcrub7Rr1N4NaxpU1Vi8sl3sCy@dpg-cvm5lu9r0fns7380utf0-a/cspb3308data")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ""
+    response_string += "<table>"
+    for basketball_player in records:
+        response_string += "<tr>"
+        for data in basketball_player: 
+            response_string += f"<td>{data}</td>"
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string
+    
