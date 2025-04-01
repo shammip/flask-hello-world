5e15f63f6803933f0aa0ca7e76995477442cd69a
shammip <shammirpereira@gmail.com>
2025-04-01 22:00:50 +0000

    create route db_insert

--- app.py
+++ app.py
@@ -29,3 +29,19 @@ def creating():
     conn.commit()
     conn.close()
     return 'Basketball Table Successfully Created'
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://cspb3308data_user:cVEDPxhcrub7Rr1N4NaxpU1Vi8sl3sCy@dpg-cvm5lu9r0fns7380utf0-a/cspb3308data")
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Succesfully Populated'
